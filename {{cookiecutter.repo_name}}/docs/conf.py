1a27e21907970ac7dd527105de68dc55f546c8cb
Jeremy Bowman <jbowman@edx.org>
2017-04-24 15:35:45 -0400

    PLAT-1241 Update dependencies

--- {{cookiecutter.repo_name}}/docs/conf.py
+++ {{cookiecutter.repo_name}}/docs/conf.py
@@ -71,7 +71,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
-    'sphinxcontrib.napoleon'
+    'sphinx.ext.napoleon'
 ]
 
 # A list of warning types to suppress arbitrary warning messages.
