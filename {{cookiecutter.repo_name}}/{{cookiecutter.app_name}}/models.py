5db85ad6d9787d459345bac81c7e8da48376a111
Jeremy Bowman <jbowman@edx.org>
2018-08-09 11:32:57 -0400

    TE-2694 Update for OEP-18, etc.

--- {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/models.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/models.py
@@ -7,7 +7,6 @@ from __future__ import absolute_import, unicode_literals
 {% if cookiecutter.models != "Comma-separated list of models" %}
 # from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
-
 from model_utils.models import TimeStampedModel
 {% for model in cookiecutter.models.replace(' ', '').split(',') %}
 
