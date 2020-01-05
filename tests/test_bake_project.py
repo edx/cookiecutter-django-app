ff2526454bd95e4e2331e7242746251ea6692aab
Ned Batchelder <ned@edx.org>
2020-01-05 09:13:05 -0500

    We don't need to delete the cookie'd results ourselves
    pytest-cookies already deletes the results as it is, and provides an
    option to keep them if we need to examine them.

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -10,7 +10,6 @@ from contextlib import contextmanager
 
 import pytest
 import sh
-from cookiecutter.utils import rmtree
 
 LOGGING_CONFIG = {
     'version': 1,
@@ -42,20 +41,6 @@ def inside_dir(dirpath):
         os.chdir(old_path)
 
 
-@contextmanager
-def bake_in_temp_dir(cookies, *args, **kwargs):
-    """
-    Delete the temporal directory that is created when executing the tests.
-
-    :param cookies: pytest_cookies.Cookies, cookie to be baked and its temporal files will be removed
-    """
-    result = cookies.bake(*args, **kwargs)
-    try:
-        yield result
-    finally:
-        rmtree(str(result.project))
-
-
 def test_bake_selecting_license(cookies):
     """Test to check if LICENSE.txt gets the correct license selected."""
     license_strings = {
@@ -63,104 +48,104 @@ def test_bake_selecting_license(cookies):
         'Apache Software License 2.0': 'Apache',
     }
     for license_name, target_string in license_strings.items():
-        with bake_in_temp_dir(cookies, extra_context={'open_source_license': license_name}) as result:
-            assert target_string in result.project.join('LICENSE.txt').read()
-            assert license_name in result.project.join('setup.py').read()
+        result = cookies.bake(extra_context={'open_source_license': license_name})
+        assert target_string in result.project.join('LICENSE.txt').read()
+        assert license_name in result.project.join('setup.py').read()
 
 
 def test_readme(cookies):
     """The generated README.rst file should pass some sanity checks and validate as a PyPI long description."""
     extra_context = {'repo_name': 'helloworld'}
-    with bake_in_temp_dir(cookies, extra_context=extra_context) as result:
+    result = cookies.bake(extra_context=extra_context)
 
-        readme_file = result.project.join('README.rst')
-        readme_lines = [x.strip() for x in readme_file.readlines(cr=False)]
-        assert 'helloworld' in readme_lines
-        assert 'The full documentation is at https://helloworld.readthedocs.org.' in readme_lines
-        setup_path = str(result.project.join('setup.py'))
-        try:
-            sh.python(setup_path, 'check', restructuredtext=True, strict=True)
-        except sh.ErrorReturnCode as exc:
-            pytest.fail(str(exc))
+    readme_file = result.project.join('README.rst')
+    readme_lines = [x.strip() for x in readme_file.readlines(cr=False)]
+    assert 'helloworld' in readme_lines
+    assert 'The full documentation is at https://helloworld.readthedocs.org.' in readme_lines
+    setup_path = str(result.project.join('setup.py'))
+    try:
+        sh.python(setup_path, 'check', restructuredtext=True, strict=True)
+    except sh.ErrorReturnCode as exc:
+        pytest.fail(str(exc))
 
 
 def test_models(cookies):
     """The generated models.py file should pass a sanity check."""
     extra_context = {'models': 'ChocolateChip, Zimsterne', 'app_name': 'cookies'}
-    with bake_in_temp_dir(cookies, extra_context=extra_context) as result:
+    result = cookies.bake(extra_context=extra_context)
 
-        model_file = result.project.join('cookies', 'models.py')
-        model_txt = model_file.read()
-        for model_name in ['ChocolateChip', 'Zimsterne']:
-            pattern = r'^class {}\(TimeStampedModel\):$'.format(model_name)
-            assert re.search(pattern, model_txt, re.MULTILINE)
+    model_file = result.project.join('cookies', 'models.py')
+    model_txt = model_file.read()
+    for model_name in ['ChocolateChip', 'Zimsterne']:
+        pattern = r'^class {}\(TimeStampedModel\):$'.format(model_name)
+        assert re.search(pattern, model_txt, re.MULTILINE)
 
 
 def test_urls(cookies):
     """The urls.py file should be present."""
     extra_context = {'app_name': 'cookies'}
-    with bake_in_temp_dir(cookies, extra_context=extra_context) as result:
-        urls_file = result.project.join('cookies', 'urls.py')
-        urls_file_txt = urls_file.read()
-        basic_url = "url(r'', TemplateView.as_view(template_name=\"cookies/base.html\"))"
-        assert basic_url in urls_file_txt
+    result = cookies.bake(extra_context=extra_context)
+    urls_file = result.project.join('cookies', 'urls.py')
+    urls_file_txt = urls_file.read()
+    basic_url = "url(r'', TemplateView.as_view(template_name=\"cookies/base.html\"))"
+    assert basic_url in urls_file_txt
 
 
 def test_travis(cookies):
     """The generated .travis.yml file should pass a sanity check."""
     extra_context = {'app_name': 'cookie_lover'}
-    with bake_in_temp_dir(cookies, extra_context=extra_context) as result:
+    result = cookies.bake(extra_context=extra_context)
 
-        travis_file = result.project.join('.travis.yml')
-        travis_text = travis_file.read()
-        assert 'pip install -r requirements/travis.txt' in travis_text
+    travis_file = result.project.join('.travis.yml')
+    travis_text = travis_file.read()
+    assert 'pip install -r requirements/travis.txt' in travis_text
 
 
 def test_app_config(cookies):
     """The generated Django AppConfig should look correct."""
     extra_context = {'app_name': 'cookie_lover'}
-    with bake_in_temp_dir(cookies, extra_context=extra_context) as result:
-        init_file = result.project.join('cookie_lover', '__init__.py')
-        pattern = r"^default_app_config = 'cookie_lover.apps.CookieLoverConfig'  #"
-        assert re.search(pattern, init_file.read(), re.MULTILINE)
+    result = cookies.bake(extra_context=extra_context)
+    init_file = result.project.join('cookie_lover', '__init__.py')
+    pattern = r"^default_app_config = 'cookie_lover.apps.CookieLoverConfig'  #"
+    assert re.search(pattern, init_file.read(), re.MULTILINE)
 
-        apps_file = result.project.join('cookie_lover', 'apps.py')
-        pattern = r'^class CookieLoverConfig\(AppConfig\):$'
-        assert re.search(pattern, apps_file.read(), re.MULTILINE)
+    apps_file = result.project.join('cookie_lover', 'apps.py')
+    pattern = r'^class CookieLoverConfig\(AppConfig\):$'
+    assert re.search(pattern, apps_file.read(), re.MULTILINE)
 
 
 def test_manifest(cookies):
     """The generated MANIFEST.in should pass a sanity check."""
     extra_context = {'app_name': 'cookie_lover'}
-    with bake_in_temp_dir(cookies, extra_context=extra_context) as result:
+    result = cookies.bake(extra_context=extra_context)
 
-        manifest_file = result.project.join('MANIFEST.in')
-        manifest_text = manifest_file.read()
-        assert 'recursive-include cookie_lover *.html *.png *.gif *js *.css *jpg *jpeg *svg *py' in manifest_text
+    manifest_file = result.project.join('MANIFEST.in')
+    manifest_text = manifest_file.read()
+    assert 'recursive-include cookie_lover *.html *.png *.gif *js *.css *jpg *jpeg *svg *py' in manifest_text
 
 
 def test_setup_py(cookies):
     """The generated setup.py should pass a sanity check."""
     extra_context = {'app_name': 'cookie_lover', 'full_name': 'Cookie McCookieface'}
-    with bake_in_temp_dir(cookies, extra_context=extra_context) as result:
+    result = cookies.bake(extra_context=extra_context)
 
-        setup_file = result.project.join('setup.py')
-        setup_text = setup_file.read()
-        assert "VERSION = get_version('cookie_lover', '__init__.py')" in setup_text
-        assert "    author='edX'," in setup_text
+    setup_file = result.project.join('setup.py')
+    setup_text = setup_file.read()
+    assert "VERSION = get_version('cookie_lover', '__init__.py')" in setup_text
+    assert "    author='edX'," in setup_text
 
 
 def test_quality_without_models(cookies):
     """The generated project should pass quality checks when no models are specified."""
-    with bake_in_temp_dir(cookies) as result:
-        check_quality(result)
+    result = cookies.bake()
+    check_quality(result)
 
 
 def test_quality_with_models(cookies):
     """The generated project should pass quality checks when models are specified."""
     extra_context = {'models': 'ChocolateChip,Zimsterne', 'app_name': 'cookies'}
-    with bake_in_temp_dir(cookies, extra_context=extra_context) as result:
-        check_quality(result)
+    result = cookies.bake(extra_context=extra_context)
+    check_quality(result)
 
 
 def check_quality(result):
@@ -202,21 +187,21 @@ def test_pii_annotations(cookies, extra_context):
     """
     Test that the pii_check make target works correctly.
     """
-    with bake_in_temp_dir(cookies, extra_context=extra_context) as result:
-        with inside_dir(str(result.project)):
-            try:
-                sh.make('upgrade')  # first run make upgrade to populate requirements/test.txt
-                sh.make('pii_check')
-            except sh.ErrorReturnCode as exc:
-                # uncovered models are expected IFF we generated any models via the cookiecutter.
-                expected_uncovered_models = 0
-                if 'models' in extra_context:
-                    # count the number of (unannotated) models the cookiecutter should generate.
-                    expected_uncovered_models = len(extra_context['models'].split(','))
-                expected_message = 'Coverage found {} uncovered models:'.format(expected_uncovered_models)
-                if expected_message not in str(exc.stdout):
-                    # First, print the stdout/stderr attrs, otherwise sh will truncate the output
-                    # guaranteeing that all we see is useless tox setup.
-                    print(exc.stdout)
-                    print(exc.stderr)
-                    pytest.fail(str(exc))
+    result = cookies.bake(extra_context=extra_context)
+    with inside_dir(str(result.project)):
+        try:
+            sh.make('upgrade')  # first run make upgrade to populate requirements/test.txt
+            sh.make('pii_check')
+        except sh.ErrorReturnCode as exc:
+            # uncovered models are expected IFF we generated any models via the cookiecutter.
+            expected_uncovered_models = 0
+            if 'models' in extra_context:
+                # count the number of (unannotated) models the cookiecutter should generate.
+                expected_uncovered_models = len(extra_context['models'].split(','))
+            expected_message = 'Coverage found {} uncovered models:'.format(expected_uncovered_models)
+            if expected_message not in str(exc.stdout):
+                # First, print the stdout/stderr attrs, otherwise sh will truncate the output
+                # guaranteeing that all we see is useless tox setup.
+                print(exc.stdout)
+                print(exc.stderr)
+                pytest.fail(str(exc))
